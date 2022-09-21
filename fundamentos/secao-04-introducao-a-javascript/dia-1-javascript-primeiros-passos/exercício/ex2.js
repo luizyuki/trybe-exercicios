4d198dc7ffde40c22c94bd89ef298912f21f939a
luizyuki <luizfyuki@gmail.com>
2022-09-21 20:40:32 -0300

    Realização do ex2.js

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercício/ex2.js
@@ -0,0 +1,12 @@
+const a = 4;
+const b = 32;
+
+if (a > b) {
+    console.log("'a' é maior que 'b'")
+}
+else if (a < b) {
+    console.log("'b' é maior que 'a'")
+}
+else {
+    console.log("'a' e 'b' são iguais")
+}

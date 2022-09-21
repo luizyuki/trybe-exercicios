db11db0b30c65fc9d9394a6d9e6580821eee35c2
luizyuki <luizfyuki@gmail.com>
2022-09-21 15:16:17 -0300

    Criação de fixar.js e exercício 1

---
+++ fundamentos/secao-04-introducao-a-javascript/fixar.js
@@ -0,0 +1,16 @@
+const myName = 'Luiz' ;
+const birthCity = 'Santos' ;
+let birthYear = 2004;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'São Vicente';
+
+console.log(birthCity);
+// Erro para trocar o valor da constante birtCity por ser uma const, tornando o valor único e não editável.

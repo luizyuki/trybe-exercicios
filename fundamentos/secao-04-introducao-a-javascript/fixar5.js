468f1aee6323d4df0d389d7050f15a45e9dad340
luizyuki <luizfyuki@gmail.com>
2022-09-21 16:14:49 -0300

    Realização do exercício 5 de Fixar

---
+++ fundamentos/secao-04-introducao-a-javascript/fixar5.js
@@ -0,0 +1,8 @@
+let weekDay = 'sábado'; // Anteriormente erá 'quarta-feira'
+
+if (weekDay === 'segunda-feira' || weekDay === 'terça-feira' || weekDay === 'quarta-feira' || weekDay ==='quinta-feira' || weekDay === 'sexta-feira') {
+    console.log('Oba, mais um dia de aprendizado na Trybe >:D')
+}
+else {
+    console.log('FINALMENTE, descanso merecido UwU')
+}

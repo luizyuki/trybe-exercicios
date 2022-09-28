e5604eae1a0ae1e075d43072274bd99bce022b86
luizyuki <luizfyuki@gmail.com>
2022-09-28 18:43:59 -0300

    Criação e Realização de fixar2.html, style.css e script.js

---
+++ fundamentos/secao-05-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,11 @@
+const sectionUrgente = document.querySelector('.emergency-tasks');
+sectionUrgente.style.backgroundColor = 'salmon';
+
+const sectionNaoUrgente = document.querySelector('.no-emergency-tasks');
+sectionNaoUrgente.style.backgroundColor = '#FEE227';
+
+document.getElementsByClassName('blockPurple')[0].style.backgroundColor = 'purple';
+document.getElementsByClassName('blockPurple')[1].style.backgroundColor = 'purple';
+
+document.getElementsByClassName('blockGray')[0].style.backgroundColor = '#1b1b1b';
+document.getElementsByClassName('blockGray')[1].style.backgroundColor = '#1b1b1b';

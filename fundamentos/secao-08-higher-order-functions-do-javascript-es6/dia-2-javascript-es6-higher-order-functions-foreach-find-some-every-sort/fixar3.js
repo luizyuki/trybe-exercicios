44269e49412703f86849e25342c76cb16b58a09e
luizyuki <luizfyuki@gmail.com>
2022-10-26 15:29:08 -0300

    Realização do fixar3

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixar3.js
@@ -0,0 +1,25 @@
+// Escreva uma função que recebe dois parâmetros: o primeiro um array de nomes e o segundo, um nome qualquer. Essa função deve retornar true se o segundo parâmetro - um nome qualquer - estiver contido no primeiro parâmetro - array de nomes. Caso não esteja contido, deve retornar false.
+
+const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
+
+const hasName = (arr, name) => {
+  return arr.some((nameI) => nameI === name);
+};
+
+console.log(hasName(names, 'Ana'));
+
+// Escreva uma função, que dado um array de pessoas e uma idade mínima, retorne true se todas tiverem a idade maior ou igual à mínima e, caso contrário, false. Use every;
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+  const verifyAges = (arr, minimumAge) => {
+    return arr.every((person) => person.age >= minimumAge);
+  };
+  
+  console.log(verifyAges(people, 18));
